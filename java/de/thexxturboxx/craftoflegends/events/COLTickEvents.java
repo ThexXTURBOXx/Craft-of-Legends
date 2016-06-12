5d0c72f7cd926771e015d2fb296408ff506c188a
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-06-12 15:39:20 +0200

    Cleanup + 1.9.4 port finished

--- java/de/thexxturboxx/craftoflegends/events/COLTickEvents.java
+++ java/de/thexxturboxx/craftoflegends/events/COLTickEvents.java
@@ -1,10 +1,5 @@
 package de.thexxturboxx.craftoflegends.events;
 
-import de.thexxturboxx.craftoflegends.api.DataManager;
-import de.thexxturboxx.craftoflegends.items.InvItem;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
-
 public class COLTickEvents {
 	
 }
