73a162e33357c7c755aae6ac2fc5f853bbe4c37d
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-06-13 20:11:08 +0200

    Cleanup

--- java/de/thexxturboxx/craftoflegends/events/COLTickEvents.java
+++ java/de/thexxturboxx/craftoflegends/events/COLTickEvents.java
@@ -1,8 +1,5 @@
 package de.thexxturboxx.craftoflegends.events;
 
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 public class COLTickEvents {
 	
 	
